cd93fa70a91fb18953d067b8438a813c1e4ba9e9
Nicholas Skinsacos <nskins@umich.edu>
2025-03-25 21:51:20 -0400

    Update Dependencies (#15)
    * Require latest version of Rust and Bevy v0.11.3
    
    * Migrate source code to handle Bevy v0.11.x changes
    
    * Update Cargo.lock for Bevy v0.11.3
    
    * Fix warnings for Bevy v0.12.1
    
    * Update Bevy dependency to v0.13.2
    
    * Migrate source code to Bevy v0.13.2
    
    * Update Bevy dependency to v0.14.2
    
    * Migrate source code for Bevy v0.14.2
    
    * Update Bevy dependency to v0.15.3
    
    * Migrate source code for Bevy v0.15.3
    
    * Fix score text to match original positioning
    
    * Remove Anchor import
    
    * Introduce common font for the two bundles
    
    * Update 'rand' dependency to v0.9.0
    
    * Update import for rand v0.9.0
    
    * Run 'cargo fmt'

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use bevy::app::AppExit;
+use bevy::audio::Volume;
 use bevy::input::keyboard::KeyboardInput;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
-use rand::prelude::*;
+use rand::random;
 
 pub const BASKET_WIDTH: f32 = 128.0;
 pub const BANANA_HEIGHT: f32 = 70.0;
@@ -11,7 +12,7 @@ pub const BANANA_SPEED: f32 = 800.0;
 
 pub const BOUND_SIZE: f32 = 120.0;
 
-pub const BACKGROUND_COLOR: Color = Color::rgb(0.6, 0.7568627451, 0.9450980392);
+pub const BACKGROUND_COLOR: Color = Color::srgb(0.6, 0.7568627451, 0.9450980392);
 
 fn main() {
     App::new()
@@ -27,20 +28,30 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .init_resource::<FallingObjectSpawnTimer>()
         .init_resource::<Score>()
-        .add_startup_system(spawn_background)
-        .add_startup_system(spawn_basket)
-        .add_startup_system(spawn_camera)
-        .add_startup_system(spawn_score_text)
-        .add_startup_system(play_music)
-        .add_startup_system(load_and_cache_images)
-        .add_system(falling_object_movement)
-        .add_system(falling_object_hit_basket)
-        .add_system(falling_object_hit_ground)
-        .add_system(tick_falling_object_spawn_timer)
-        .add_system(spawn_falling_objects_over_time)
-        .add_system(update_basket_position)
-        .add_system(update_score)
-        .add_system(close_on_escape)
+        .add_systems(
+            Startup,
+            (
+                spawn_background,
+                spawn_basket,
+                spawn_camera,
+                spawn_score_text,
+                play_music,
+                load_and_cache_images,
+            ),
+        )
+        .add_systems(
+            Update,
+            (
+                falling_object_movement,
+                falling_object_hit_basket,
+                falling_object_hit_ground,
+                tick_falling_object_spawn_timer,
+                spawn_falling_objects_over_time,
+                update_basket_position,
+                update_score,
+                close_on_escape,
+            ),
+        )
         .run();
 }
 
@@ -60,7 +71,7 @@ pub enum FallingObjectKind {
 pub struct Basket {}
 
 #[derive(Component)]
-pub struct ScoreText {}
+pub struct ScoreText;
 
 #[derive(Resource)]
 pub struct FallingObjectSpawnTimer {
@@ -105,7 +116,7 @@ pub fn falling_object_movement(
 ) {
     for mut transform in object_query.iter_mut() {
         let direction = Vec3::new(0.0, -1.0, 0.0);
-        transform.translation += direction * BANANA_SPEED * time.delta_seconds();
+        transform.translation += direction * BANANA_SPEED * time.delta_secs();
     }
 }
 
@@ -163,11 +174,8 @@ pub fn spawn_falling_objects_over_time(
         };
 
         commands.spawn((
-            SpriteBundle {
-                transform: Transform::from_xyz(random_x, window.height(), 0.0),
-                texture,
-                ..default()
-            },
+            Sprite::from_image(texture),
+            Transform::from_xyz(random_x, window.height(), 0.0),
             FallingObject {
                 kind: object_kind,
                 points,
@@ -193,11 +201,11 @@ pub fn falling_object_hit_ground(
     }
 }
 
-pub fn play_music(asset_server: Res<AssetServer>, audio: Res<Audio>) {
-    audio.play_with_settings(
-        asset_server.load("audio/main_theme.ogg"),
-        PlaybackSettings::LOOP.with_volume(1.0),
-    );
+pub fn play_music(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        AudioPlayer::<AudioSource>(asset_server.load("audio/main_theme.ogg")),
+        PlaybackSettings::LOOP.with_volume(Volume::new(1.0)),
+    ));
 }
 
 pub fn spawn_background(
@@ -207,35 +215,30 @@ pub fn spawn_background(
 ) {
     let window = window_query.get_single().unwrap();
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 6.0, window.height() / 1.25, -1.0),
-        texture: asset_server.load("sprites/hot_air_balloon.png"),
-        ..default()
-    });
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/hot_air_balloon.png")),
+        Transform::from_xyz(window.width() / 6.0, window.height() / 1.25, -1.0),
+    ));
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 1.5, window.height() / 4.5, -1.0),
-        texture: asset_server.load("sprites/tree.png"),
-        ..default()
-    });
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/tree.png")),
+        Transform::from_xyz(window.width() / 1.5, window.height() / 4.5, -1.0),
+    ));
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 2.5, window.height() / 1.75, -1.0),
-        texture: asset_server.load("sprites/cloud_small.png"),
-        ..default()
-    });
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/cloud_small.png")),
+        Transform::from_xyz(window.width() / 2.5, window.height() / 1.75, -1.0),
+    ));
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 1.1, window.height(), -1.0),
-        texture: asset_server.load("sprites/cloud_big.png"),
-        ..default()
-    });
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/cloud_big.png")),
+        Transform::from_xyz(window.width() / 1.1, window.height(), -1.0),
+    ));
 
-    commands.spawn(SpriteBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, 119.0 / 2.0, -2.0),
-        texture: asset_server.load("sprites/ground.png"),
-        ..default()
-    });
+    commands.spawn((
+        Sprite::from_image(asset_server.load("sprites/ground.png")),
+        Transform::from_xyz(window.width() / 2.0, 119.0 / 2.0, -2.0),
+    ));
 }
 
 pub fn spawn_basket(
@@ -246,11 +249,8 @@ pub fn spawn_basket(
     let window = window_query.get_single().unwrap();
 
     commands.spawn((
-        SpriteBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, BOUND_SIZE, 0.0),
-            texture: asset_server.load("sprites/basket.png"),
-            ..default()
-        },
+        Sprite::from_image(asset_server.load("sprites/basket.png")),
+        Transform::from_xyz(window.width() / 2.0, BOUND_SIZE, 0.0),
         Basket {},
     ));
 }
@@ -258,37 +258,37 @@ pub fn spawn_basket(
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
 
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        ..default()
-    });
+    commands.spawn((
+        Camera2d,
+        Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+    ));
 }
 
-pub fn spawn_score_text(mut commands: Commands, asset_server: Res<AssetServer>, score: Res<Score>) {
-    commands.spawn((
-        TextBundle {
-            text: Text::from_section(
-                format!("Score: {0}", score.value),
-                TextStyle {
-                    font: asset_server.load("fonts/FiraMono-Medium.ttf"),
-                    font_size: 60.0,
-                    color: Color::BLACK,
-                    ..default()
-                },
-            ),
-            ..default()
-        }
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            position: UiRect {
+pub fn spawn_score_text(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = TextFont {
+        font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+        font_size: 60.0,
+        ..default()
+    };
+
+    commands
+        .spawn((
+            Text::new("Score: "),
+            font.clone(),
+            Node {
+                position_type: PositionType::Absolute,
                 top: Val::Px(5.0),
                 left: Val::Px(15.0),
                 ..default()
             },
-            ..default()
-        }),
-        ScoreText {},
-    ));
+            TextColor::BLACK,
+        ))
+        .with_child((
+            TextSpan::default(),
+            font.clone(),
+            TextColor::BLACK,
+            ScoreText,
+        ));
 }
 
 pub fn tick_falling_object_spawn_timer(
@@ -306,7 +306,7 @@ pub fn update_basket_position(
     let window = window_query.get_single().unwrap();
 
     if let Ok(mut transform) = basket_query.get_single_mut() {
-        for event in events.iter() {
+        for event in events.read() {
             if event.position.x < BOUND_SIZE {
                 transform.translation.x = BOUND_SIZE;
             } else if event.position.x > window.width() - BOUND_SIZE {
@@ -318,10 +318,13 @@ pub fn update_basket_position(
     }
 }
 
-pub fn update_score(mut score_text_query: Query<&mut Text, With<ScoreText>>, score: Res<Score>) {
+pub fn update_score(
+    mut score_text_query: Query<&mut TextSpan, With<ScoreText>>,
+    score: Res<Score>,
+) {
     if score.is_changed() {
-        if let Ok(mut text) = score_text_query.get_single_mut() {
-            text.sections[0].value = format!("Score: {0}", score.value);
+        for mut text in &mut score_text_query {
+            **text = format!("{0}", score.value);
         }
     }
 }
@@ -330,9 +333,9 @@ fn close_on_escape(
     mut keyboard_input_events: EventReader<KeyboardInput>,
     mut app_exit_events: EventWriter<AppExit>,
 ) {
-    for event in keyboard_input_events.iter() {
-        if event.key_code == Some(KeyCode::Escape) && event.state.is_pressed() {
-            app_exit_events.send(AppExit);
+    for event in keyboard_input_events.read() {
+        if event.key_code == KeyCode::Escape && event.state.is_pressed() {
+            app_exit_events.send(AppExit::Success);
         }
     }
 }
